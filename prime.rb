5cf7037a4c943f8ad8f9cbac495a2954b350cd93
cd1412 <chiduong1412@gmail.com>
2022-02-24 23:34:36 -0600

    Completed assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(integer)
+    return false if integer < 2
+        range = 2..integer - 1
+        range.each do |i|
+            return false if integer % i == 0
+        end
+        true
+end
